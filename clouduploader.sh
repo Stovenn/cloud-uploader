631be7454a3248c83836e8b31209fe40221d5ab7
Jonathan Sylvestre <jonathan.sylvestre.pro@gmail.com>
2024-07-27 23:37:47 +0200

    finish script

--- clouduploader.sh
+++ clouduploader.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-
 FILE_PATH=$1
+CONTAINER_NAME=$2
 
 if [ ! -f $FILE_PATH ]; then
-echo $FILE_PATH does not exists
-exit 1
+    echo $FILE_PATH not found
+    exit 1
 fi
 
-echo $FILE_PATH exists
+az storage blob upload -f $FILE_PATH -c $CONTAINER_NAME
+if [ $? -eq 0 ]; then
+    echo File successfully upload to $CONTAINER_NAME
+else
+    echo Error while uploading the file
+fi
